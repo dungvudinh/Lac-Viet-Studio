c960f9345383fb97255ff2fdd526bab92b8d9a16
dungvudinh <anhkho881@gmail.com>
2025-04-16 16:17:14 +0700

    config base url

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react() 
-  ],
-  resolve:{
-    alias:[
-      { find:'~', replacement:'/src' }
+  base: '/', // <-- make sure this is here
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '~', replacement: '/src' }
     ]
-  }, 
+  },
   server: {
     proxy: {
       '/v1': {
         target: 'https://lac-viet-studio-api.onrender.com',
-        changeOrigin:true,
-        secure:true
+        changeOrigin: true,
+        secure: true,
       }
     }
   }
