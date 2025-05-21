ddeb346fd91532e2d9a5734b03a874e21aec1f6d
dungvudinh <anhkho881@gmail.com>
2025-05-21 11:14:33 +0700

    config file build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/', // <-- make sure this is here
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }, 
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
